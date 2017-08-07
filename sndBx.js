ae8612ce6c18a47b199ca23bc3be5e1480e1cfd5
seedubgh1 <carltonsaddress@hotmail.com>
2017-08-07 12:48:10 -0700

    commit latest sndBx.js

--- sndBx.js
+++ sndBx.js
@@ -1,31 +1,32 @@
-var chkPoints = 2,
+var chkPoints = 3,
     chkPointPcnt = 0,
-    gameLength = 17,
+    gameLength = 7,
     currQuestion = 6,
 		progress = 0;
 
+console.log('Checkpoints: ',chkPoints);
 chkPoints += 1;
-console.log('Total checks: ',chkPoints);
 
 chkPointPcnt = 100/chkPoints;
-console.log('Chkpoint percent: ',chkPointPcnt);
 chkPointPcnt = parseInt(chkPointPcnt);
 console.log('Chkpoint percent: ',chkPointPcnt);
 
+console.log('Questions answered: ', currQuestion);
+console.log('Total questions: ', gameLength);
 progress = currQuestion/gameLength;
-console.log('Progress: ',progress);
-
 progress = parseInt(progress * 100);
-console.log('Progress: ', progress);
+console.log('Progress: ', progress + '%');
 
-var idxs = new Array(3);
-var chks = [];
-idxs.forEach((num) => chks.push(1));
-
-console.log(chks);
-
-chks = Array.from(Array(chkPoints).keys());
+chks = Array.from(Array(chkPoints).keys())
 chks.shift();
-console.log('chks: ', chks);
 chks = chks.map((num) => num * chkPointPcnt);
 console.log('chksPcnt', chks);
+
+var nxtChk = chks.shift();
+console.log('Next Check: ',nxtChk);
+nxtChk = chks.shift();
+console.log('Next Check: ',nxtChk);
+nxtChk = chks.shift();
+console.log('Next Check: ',nxtChk);
+nxtChk = chks.shift();
+console.log('Next Check: ',nxtChk);
