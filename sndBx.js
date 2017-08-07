c9534ca0ab33954e8ed29a42177107e707ff8adb
seedubgh1 <carltonsaddress@hotmail.com>
2017-08-07 07:47:31 -0700

    commit sndBx.js

--- sndBx.js
+++ sndBx.js
@@ -1,17 +1,31 @@
-var steps = ["brainstorm", "narrow", "prototype", "test", "propose"];
-// var stepTerm;
+var chkPoints = 2,
+    chkPointPcnt = 0,
+    gameLength = 17,
+    currQuestion = 6,
+		progress = 0;
 
-steps.forEach(function(value,index,array) {
-  var stepTerm
-	switch(index) {
-		case 0:
-				stepTerm = "First";
-				break;
-		case array.length -1:
-				stepTerm	= "Finally";
-				break;
-		default:
-				stepTerm = "Then";
-	}
-console.log(stepTerm + " we " + array[index]);
-});
+chkPoints += 1;
+console.log('Total checks: ',chkPoints);
+
+chkPointPcnt = 100/chkPoints;
+console.log('Chkpoint percent: ',chkPointPcnt);
+chkPointPcnt = parseInt(chkPointPcnt);
+console.log('Chkpoint percent: ',chkPointPcnt);
+
+progress = currQuestion/gameLength;
+console.log('Progress: ',progress);
+
+progress = parseInt(progress * 100);
+console.log('Progress: ', progress);
+
+var idxs = new Array(3);
+var chks = [];
+idxs.forEach((num) => chks.push(1));
+
+console.log(chks);
+
+chks = Array.from(Array(chkPoints).keys());
+chks.shift();
+console.log('chks: ', chks);
+chks = chks.map((num) => num * chkPointPcnt);
+console.log('chksPcnt', chks);
